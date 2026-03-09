06fb432824edfb5e3068ce90403a770b7e7bc2f2
Ayoubedf <ayoubelouadifi2005@gmail.com>
2026-03-09 00:03:45 +0000

    Add role property to UserDto

--- src/main/java/com/hightech/university/modules/user/models/dto/UserDto.java
+++ src/main/java/com/hightech/university/modules/user/models/dto/UserDto.java
@@ -1,15 +1,17 @@
 package com.hightech.university.modules.user.models.dto;
 
+import com.hightech.university.modules.user.models.entity.Role;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 
 import java.util.UUID;
 
-@Getter
+@Data
 @AllArgsConstructor
 public class UserDto {
     private UUID id;
     private String email;
     private String firstName;
     private String lastName;
+    private Role role;
 }
