b5958201aaa266fa474e246125eb6d0252569e26
ayoub <ayoubelouadifi2005@gmail.com>
2026-03-12 17:18:53 +0000

    Add RegisterRequest DTO

---
+++ src/main/java/com/hightech/university/modules/auth/models/dto/RegisterRequest.java
@@ -0,0 +1,31 @@
+package com.hightech.university.modules.auth.models.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RegisterRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&.]).{8,}$",
+            message = "Password must be at least 8 characters long and include letters, numbers, and a special character."
+    )
+    private String password;
+
+    @NotBlank
+    @Size(min = 2)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 2)
+    private String lastName;
+}
